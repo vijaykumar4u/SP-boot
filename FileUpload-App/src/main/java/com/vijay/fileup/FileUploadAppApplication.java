4a64d8e7e59c07472a32a958c2eba5de1a4d26b0
lenovo <lenovo@DESKTOP-JOQO55T>
2020-12-06 21:15:53 +0530

    file upload

---
+++ FileUpload-App/src/main/java/com/vijay/fileup/FileUploadAppApplication.java
@@ -0,0 +1,14 @@
+package com.vijay.fileup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadAppApplication.class, args);
+	}
+	
+
+}
---
+++ FileUpload-App/src/test/java/com/vijay/fileup/FileUploadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.fileup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
