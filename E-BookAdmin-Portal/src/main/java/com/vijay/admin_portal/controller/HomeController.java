4b33ca501a22ee8e45191442c0a481a292dda75f
lenovo <lenovo@DESKTOP-JOQO55T>
2020-12-06 21:13:43 +0530

    initial commit

---
+++ E-BookAdmin-Portal/src/main/java/com/vijay/admin_portal/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.vijay.admin_portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "index";
+
+	}
+
+}
