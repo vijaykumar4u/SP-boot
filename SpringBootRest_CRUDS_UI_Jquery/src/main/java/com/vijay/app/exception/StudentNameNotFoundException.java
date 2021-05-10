f3ebfa1f6f8a5c218909be8a0cce0fd037d4acab
vijaykumar4u <74670977+vijaykumar4u@users.noreply.github.com>
2021-05-10 07:54:55 +0530

    Add files via upload

---
+++ SpringBootRest_CRUDS_UI_Jquery/src/main/java/com/vijay/app/exception/StudentNameNotFoundException.java
@@ -0,0 +1,32 @@
+package com.vijay.app.exception;
+
+public class StudentNameNotFoundException extends RuntimeException{
+
+	public StudentNameNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentNameNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentNameNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentNameNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentNameNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
